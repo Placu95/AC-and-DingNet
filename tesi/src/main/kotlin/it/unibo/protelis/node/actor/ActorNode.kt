3c458abc4daba65a03c4fde3d48bd7f23805c64a
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-09 13:15:11 +0200

    add actor that execute protelisVM

---
+++ tesi/src/main/kotlin/it/unibo/protelis/node/actor/ActorNode.kt
@@ -0,0 +1,60 @@
+package it.unibo.protelis.node.actor
+
+import akka.actor.AbstractActorWithTimers
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.japi.pf.ReceiveBuilder
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+import it.unibo.protelis.model.GPSPosition
+import it.unibo.protelis.model.SensorType
+import it.unibo.protelis.node.DestinationNode
+import it.unibo.protelis.node.GenericNode
+import it.unibo.protelis.node.SensorNode
+import it.unibo.protelis.node.UserNode
+import org.protelis.lang.datatype.impl.StringUID
+import org.protelis.vm.ProtelisProgram
+import java.io.InputStreamReader
+import java.time.Duration
+
+
+class TimerTick
+
+class ActorNode(val genericNode: GenericNode): AbstractActorWithTimers() {
+
+    override fun preStart() {
+        super.preStart()
+        timers.startPeriodicTimer(genericNode.deviceUID, TimerTick(), Duration.ofMillis(genericNode.sleepTime))
+    }
+
+    override fun createReceive(): Receive =
+        ReceiveBuilder.create()
+            .match(TimerTick::class.java) { genericNode.runVM() }
+            .build()
+}
+
+object ActorNodeFactory {
+
+    const val actorSystemName = "protelisNodes"
+    private const val configPath = "/configuration/actor/LocalActor.conf"
+    private val localConf: Config by lazy { ConfigFactory.parseReader(
+        InputStreamReader(
+            ActorNodeFactory::class.java.getResourceAsStream(configPath)))
+    }
+    private val actorSystem by lazy { ActorSystem.create(actorSystemName, localConf) }
+
+    fun createDestinationNode(protelisProgram: ProtelisProgram, sleepTime: Long, mqttAddress: String, applicationUID: String,
+                              destinationUID: StringUID, position: GPSPosition) =
+        createActorWithNode(DestinationNode(protelisProgram, sleepTime, destinationUID, applicationUID, mqttAddress, position))
+
+    fun createSensorNode(protelisProgram: ProtelisProgram, sleepTime: Long, mqttAddress: String, applicationUID: String,
+                         sensorDeviceUID: StringUID, position: GPSPosition, sensorTypes: List<SensorType>) =
+        createActorWithNode(SensorNode(protelisProgram, sleepTime, sensorDeviceUID, applicationUID, mqttAddress, position, sensorTypes))
+
+    fun createUserNode(protelisProgram: ProtelisProgram, sleepTime: Long, mqttAddress: String, applicationUID: String,
+                      userUID: StringUID, position: GPSPosition, sensorTypes: List<SensorType> = emptyList()) =
+        createActorWithNode(UserNode(protelisProgram, sleepTime, userUID, applicationUID, mqttAddress, position, sensorTypes))
+
+
+    fun createActorWithNode(genericNode: GenericNode) = actorSystem.actorOf(Props.create(ActorNode::class.java) { ActorNode(genericNode) })
+}
