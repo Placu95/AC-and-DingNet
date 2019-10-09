d6be2bedcf7d6519661993d5a09e12e9e8408ead
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-09 13:12:59 +0200

    add akka dependency

--- tesi/build.gradle.kts
+++ tesi/build.gradle.kts
@@ -9,4 +9,5 @@ dependencies {
     implementation("org.protelis:protelis:${extra["protelisVersion"].toString()}")
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:${extra["paho_version"].toString()}")
     implementation("com.google.code.gson:gson:${extra["gsonVersion"].toString()}")
+    implementation("com.typesafe.akka:akka-actor_2.12:${extra["akkaVersion"].toString()}")
 }
