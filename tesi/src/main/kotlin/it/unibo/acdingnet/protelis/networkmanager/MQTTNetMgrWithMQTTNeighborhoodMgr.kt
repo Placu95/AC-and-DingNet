6a05e0fd57b77cc7da25eb4d0f0a66437315e024
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-10 14:49:07 +0200

    add class MQTTNetMgrWithMQTTNeighborhoodMgr

---
+++ tesi/src/main/kotlin/it/unibo/acdingnet/protelis/networkmanager/MQTTNetMgrWithMQTTNeighborhoodMgr.kt
@@ -0,0 +1,47 @@
+package it.unibo.acdingnet.protelis.networkmanager
+
+import it.unibo.acdingnet.protelis.model.LatLongPosition
+import it.unibo.acdingnet.protelis.neighborhood.NeighborhoodMessage
+import it.unibo.acdingnet.protelis.neighborhood.NeighborhoodMessage.MessageType
+import it.unibo.acdingnet.protelis.neighborhood.NewNeighborhoodMessage
+import it.unibo.acdingnet.protelis.neighborhood.Node
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.protelis.lang.datatype.impl.StringUID
+
+class MQTTNetMgrWithMQTTNeighborhoodMgr(
+    deviceUID: StringUID,
+    serverAddress: String,
+    applicationEUI: String,
+    initialPosition: LatLongPosition
+) : MQTTNetworkManager(deviceUID, serverAddress, applicationEUI, emptySet()) {
+
+    private val publishOnTopic: String = "application/$applicationEUI/neighborhoodManager"
+    private val subscribeOnTopic: String = "$baseTopic${deviceUID.uid}/neighborhood"
+
+    init {
+        mqttClient.subscribe(subscribeOnTopic) {
+            _, message ->  setNeighbors(neighbors = gson.fromJson("$message", NewNeighborhoodMessage::class.java)
+                .neighborhood.map { it.uid }.toSet())
+        }
+
+        mqttClient.publish(
+            publishOnTopic,
+            generateMessage(MessageType.ADD, deviceUID, initialPosition)
+        )
+    }
+
+    fun changePosition(position: LatLongPosition) {
+        mqttClient.publish(publishOnTopic, generateMessage(MessageType.UPDATE, deviceUID, position))
+    }
+
+    fun nodeDeleated() {
+        mqttClient.publish(publishOnTopic, generateMessage(MessageType.LEAVE, deviceUID, LatLongPosition.zero()))
+    }
+
+    private fun generateMessage(type: MessageType, uid: StringUID, position: LatLongPosition) =
+        generateMessage(type, Node(uid, position))
+
+    private fun generateMessage(type: MessageType, node: Node) =
+        MqttMessage(gson.toJson(NeighborhoodMessage(type, node)).toByteArray(Charsets.US_ASCII))
+
+}
