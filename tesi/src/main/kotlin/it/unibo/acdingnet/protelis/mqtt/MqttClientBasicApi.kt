412e867e33fe8534ac86d97cf3efb4d54d30f3e4
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-11 10:31:31 +0200

    add basic interface for mqtt communication

---
+++ tesi/src/main/kotlin/it/unibo/acdingnet/protelis/mqtt/MqttClientBasicApi.kt
@@ -0,0 +1,16 @@
+package it.unibo.acdingnet.protelis.mqtt
+
+interface MqttClientBasicApi {
+
+    fun connect()
+
+    fun disconnect()
+
+    fun publish(topic: String, message: String)
+
+    fun publish(topic: String, message: ByteArray)
+
+    fun subscribe(topicFilter: String, messageListener: (topic: String, message: String) -> Unit)
+
+    fun unsubscribe(topicFilter: String)
+}
