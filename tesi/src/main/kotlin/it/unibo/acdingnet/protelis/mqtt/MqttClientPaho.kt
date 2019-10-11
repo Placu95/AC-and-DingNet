dad6f5a79affac724a3e548a4f355cd1f0461d76
Placu <andrea.placuzzi@studio.unibo.it>
2019-10-11 10:32:12 +0200

    add wrapper of eclipse mqtt implementation

---
+++ tesi/src/main/kotlin/it/unibo/acdingnet/protelis/mqtt/MqttClientPaho.kt
@@ -0,0 +1,24 @@
+package it.unibo.acdingnet.protelis.mqtt
+
+import org.eclipse.paho.client.mqttv3.MqttClient
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
+import org.eclipse.paho.client.mqttv3.MqttMessage
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+
+class MqttClientPaho(mqttAddress: String, clientId: String) : MqttClientBasicApi{
+
+    private val mqttClient = MqttClient(mqttAddress, clientId, MemoryPersistence())
+
+    override fun connect() = mqttClient.connect(MqttConnectOptions().also { it.isCleanSession = true })
+
+    override fun disconnect() = mqttClient.disconnect()
+
+    override fun publish(topic: String, message: String) = publish(topic, message.toByteArray(Charsets.US_ASCII))
+
+    override fun publish(topic: String, message: ByteArray) = mqttClient.publish(topic, MqttMessage(message))
+
+    override fun subscribe(topicFilter: String, messageListener: (topic: String, message: String) -> Unit) =
+        mqttClient.subscribe(topicFilter) {topic, message -> messageListener(topic, "$message") }
+
+    override fun unsubscribe(topicFilter: String) = mqttClient.unsubscribe(topicFilter)
+}
