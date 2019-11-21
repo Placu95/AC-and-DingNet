aa0b5e3f02d66c77307477bdf32de83745e198df
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-21 15:37:47 +0100

    test update environment after packet received

--- tesi/src/test/kotlin/it/unibo/acdingnet/protelis/executioncontext/SensorExecutionContextTest.kt
+++ tesi/src/test/kotlin/it/unibo/acdingnet/protelis/executioncontext/SensorExecutionContextTest.kt
@@ -37,6 +37,14 @@ class SensorExecutionContextTest : StringSpec() {
     init {
 
         "when arrive a message from mqtt the execution context should update the node information" {
+
+            val newPos = LatLongPosition(1.0, 1.0)
+            val sensorData: Map<SensorType, List<Byte>> = mapOf(
+                SensorType.SOOT to listOf<Byte>(50),
+                SensorType.GPS to newPos.toBytes(),
+                SensorType.CO2 to listOf<Byte>(89))
+
+            val sensors = sensorData.keys.toList()
             val node = SensorNodeSpy(
                 EmptyProtelisProgram(),
                 1L,
@@ -44,15 +52,16 @@ class SensorExecutionContextTest : StringSpec() {
                 "test",
                 MqttClientMock(),
                 LatLongPosition.zero(),
-                listOf(SensorType.SOOT)
+                sensors
             )
 
-            should { !node.spyExecContext().executionEnvironment.has(SensorType.SOOT.type) }
+            should { sensors.none { node.spyExecContext().executionEnvironment.has(it.type) }}
             node.position shouldBe LatLongPosition.zero()
 
             //generate message
             val header = FrameHeader(emptyList(), 0,0, emptyList())
-            val packet = LoRaWanPacket(1001L, 1L, false, 0.8, 15, emptyList(), emptyList(), header)
+            val data: List<Byte> = sensorData.values.flatten().toList()
+            val packet = LoRaWanPacket(1001L, 1L, false, 0.8, 15, data, emptyList(), header)
             val transmission = LoRaTransmission(1001L, 2L, 0.5, 60, 790, packet, "DATA_RATE1", LocalTime.MIN, 385.4,
                 arrived = true,
                 collided = false
@@ -60,8 +69,15 @@ class SensorExecutionContextTest : StringSpec() {
             //send message
             MqttClientMock().publish("application/${node.applicationUID}/node/${node.deviceUID.uid}/rx", LoRaTransmissionWrapper(transmission))
             //check value update
-            //node.spyExecContext().executionEnvironment.get(SensorType.SOOT.type) as Double shouldBe data.sensorValue
-            //node.position shouldBe newPos
+            should {
+                val execEnv = node.spyExecContext().executionEnvironment
+                sensors
+                    .filter { it != SensorType.GPS }
+                    .all {
+                        execEnv.get(it.type) as Double == sensorData[it]?.get(0)?.toDouble()
+                }
+            }
+            node.position shouldBe newPos
         }
     }
 }
