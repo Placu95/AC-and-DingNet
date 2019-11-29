681fc2a4c215ec7596d1c52d014ce73057e2e076
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-29 18:52:10 +0100

    add const object

---
+++ tesi/src/main/kotlin/it/unibo/acdingnet/protelis/util/Const.kt
@@ -0,0 +1,10 @@
+package it.unibo.acdingnet.protelis.util
+
+object Const {
+
+    const val APPLICATION_ID = "1"
+    const val NEIGHBORHOOD_RANGE = 50.0
+    const val MOTT_ADDRESS = "tcp://test.mosquitto.org:1883"
+    const val MQTT_CLIENT_ID = "testFenomeno1995-app"
+    const val IAQLEVEL_KEY = "iaqLevel"
+}
