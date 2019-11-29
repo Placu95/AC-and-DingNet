481723cb8c404a369173b173aeb8d657f7ab5550
Placu <andrea.placuzzi@studio.unibo.it>
2019-11-29 18:53:10 +0100

    add sensor for compatibility with DingNetSimulator

--- tesi/src/main/kotlin/it/unibo/acdingnet/protelis/model/SensorType.kt
+++ tesi/src/main/kotlin/it/unibo/acdingnet/protelis/model/SensorType.kt
@@ -7,7 +7,8 @@ import it.unibo.acdingnet.protelis.model.sensorconverter.UnsupportedConversion
 enum class SensorType(val lenght: Int = 1, private val converter: SensorConverter = DefaultConverter()) {
     GPS(8, UnsupportedConversion()),
     PM10,
-    NO2(2);
+    NO2(2),
+    IAQ(0, UnsupportedConversion());
 
     fun convertToDouble(data: MutableList<Byte>): Double = converter.convert(lenght, data)
 
